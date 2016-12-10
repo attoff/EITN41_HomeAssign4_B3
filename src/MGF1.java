f31ab3e793f4c1fe5687360ab890f8ac01419650
astrimagnussen <dic12ama@student.lu.se>
2016-12-10 13:55:00 +0100

    mgf1 fixed, only sha1 left

--- src/MGF1.java
+++ src/MGF1.java
@@ -1,24 +1,38 @@
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 
 public class MGF1 {
     private MessageDigest md;
+    private String mfgSeed;
+    private int maskLen;
+
 
     public MGF1(String mfgSeed, int maskLen) {
-
+        this.mfgSeed = mfgSeed;
+        this.maskLen = maskLen;
         if (maskLen > Math.pow(2, 32)) {
             System.out.println("Mask too long");
             System.exit(0);
         }
+    }
+    public String calculate(){
 
         String t = "";
 
         int iter = (int) Math.ceil((maskLen/md.getDigestLength())-1);
         for (int i = 0; i < iter; i++) {
-            t = I2OSP(i, 4);
+            String c = I2OSP(i, 4);
+            String temp = mfgSeed.concat(c);
+            try {
+                md = MessageDigest.getInstance("SHA-1");
+                md.update(temp.getBytes("UTF-8"));
+                //fixa så att sha fungerar, och hasha temp och gör till string
+                t = t.concat(temp);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
-
+        return t.substring(0, maskLen*2);
     }
 
     private String I2OSP(int x, int xLen) {
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@
 public class Main {
 
     public static void main(String[] args) {
+        String mgfSeed = "0123456789abcdef";
+        int maskLen = 30;
+        MGF1 mg = new MGF1(mgfSeed, maskLen);
+        String output = mg.calculate();
+        System.out.println(output);
 
     }
 }
