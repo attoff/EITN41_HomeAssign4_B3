e3ad5c597d6bb5a4bb744249ee124b168a8f9440
attoff <viktor.attoff@gmail.com>
2016-12-11 12:39:20 +0100

    whatver

--- src/MGF1.java
+++ src/MGF1.java
@@ -1,10 +1,24 @@
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 
 public class MGF1 {
     private MessageDigest md;
+    BigInteger seed;
+    String seed_str;
 
     public MGF1(String mfgSeed, int maskLen) {
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+            seed = new BigInteger(mfgSeed, 16);
+            this.seed_str = mfgSeed;
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
 
         if (maskLen > Math.pow(2, 32)) {
             System.out.println("Mask too long");
@@ -13,12 +27,29 @@ public class MGF1 {
 
         String t = "";
 
-        int iter = (int) Math.ceil((maskLen/md.getDigestLength())-1);
-        for (int i = 0; i < iter; i++) {
-            t = I2OSP(i, 4);
+        int iter = (int) (Math.ceil((double) maskLen / (double) md.getDigestLength()) - 1);
+        System.out.println(maskLen);
+        System.out.println(md.getDigestLength());
+        System.out.println(iter);
+        for (int i = 0; i <= iter; i++) {
+            String c = I2OSP(i, 4);
+            System.out.println("C: " + c);
+
+            c = seed_str + c;
+
+            String comb = seed_str.concat(c);
+
+
+            md.update(comb.getBytes());
+
+
+            byte[] hash = md.digest();
+
+            String hex = byteToHex(hash);
+            t = t + hex;
         }
 
-
+        System.out.println("Output: " + t);
     }
 
     private String I2OSP(int x, int xLen) {
@@ -28,8 +59,18 @@ public class MGF1 {
         temp[1] = (byte) (x >>> 16);
         temp[2] = (byte) (x >>> 8);
         temp[3] = (byte) (x >>> 0);
-        
-        return "";
+
+        return byteToHex(temp);
+    }
+
+    private static String byteToHex(byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        MGF1 mfg1 = new MGF1("123456789abcdef" , 30);
 
     }
 }
